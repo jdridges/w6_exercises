117b867d24a4485d139ea84ac87b0d7d62abc101
jdridges <jridges@appdev.yearup.org>
2024-10-29 15:51:53 -0500

    added beginning of lab 3 exercise 1b

---
+++ Functions/math_and_stats.py
@@ -0,0 +1,17 @@
+import random 
+import math
+import statistics
+
+vals1_100 = range(1,100)
+vals_sample = random.sample(vals1_100, 75)
+vals_choices = random.choices(vals1_100, k = 200)
+radius = random.randint(3,10)
+pi = math.pi
+
+#sum of sample 75 samples from 1 to 100:
+print(math.fsum(vals_sample))
+
+#avg of 75 sample values
+print(statistics.mean(vals_sample))
+
+
