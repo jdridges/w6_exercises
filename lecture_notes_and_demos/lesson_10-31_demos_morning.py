7e1971313282d98245c95aa178c2e65676dd32df
jdridges <jridges@appdev.yearup.org>
2024-10-31 09:34:14 -0500

    updated demo

--- lecture_notes_and_demos/lesson_10-31_demos_morning.py
+++ lecture_notes_and_demos/lesson_10-31_demos_morning.py
@@ -4,9 +4,52 @@ movies = [
     ('Spiderman: Far from Home', 'Despicable Me 2', 'Freedom Writers')
     ]
 
-print(f'I\'ve heard {movies[1][2]} is a good movie')
 
-for x in movies:
-    for i in x:
-        print(f'{i} is a good movie')       #for tuples in a list
+
+
+        #CHANGING TUPLES TO LISTS TO MAKE CHANGES 
         
+m1, m2, m3 = movies
+change_m1 = list(m1)
+
+change_m1.append('The Last Unicorn')
+
+# print(change_m1)
+
+m1 = tuple(change_m1)
+
+movies[0] = m1
+
+# print(movies)
+
+change_m2 = list(m2)
+
+change_m2[1] = 'The Fall guy'
+
+m2 = tuple(change_m2)
+
+movies[1] = m2
+
+print(movies)
+
+
+
+
+
+
+
+# print(f'I\'ve heard {movies[1][2]} is a good movie')
+
+# for x in movies:
+#     for i in x:
+#         print(f'{i} is a good movie')       #for tuples in a list
+
+
+# set4= movies[0]
+# set5 = movies[1]
+# set6 = movies[2]
+# print(set4)
+
+# movie1, movie2, movie3 = set4
+# print(movie3)
+# print(movies[0][2])
