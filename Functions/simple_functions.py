1330b541e592fcb5824ec0f71308d36e2df9b850
jdridges <jridges@appdev.yearup.org>
2024-11-01 12:26:43 -0500

    completed simple_functions exercise using both print and return statements

---
+++ Functions/simple_functions.py
@@ -0,0 +1,18 @@
+#LAB1 #Q2
+def favorite_things(name, favorite_movie, favorite_band):
+   return f'{name}\'s favorite movie is {favorite_movie} and favorite band is {favorite_band}'
+
+print(favorite_things('Jacob', 'Kicking and Screaming', 'Tame Impala'))
+
+#LAB1 Q3
+def why_im_here():
+  print(f'I am here in order to kickstart my career in Data Analytics and to learn Python!')
+
+why_im_here()
+
+#LAB1 Q4
+def favorite_places(place, reason_for_liking_place):
+   return f'{place} is my favorite place in the world because {reason_for_liking_place}'
+
+print(favorite_places('North Carolina', 'it is a family tradition to go every year and the beach is beautiful'))
+print(favorite_places('San Diego', 'it is warm all the time'))
