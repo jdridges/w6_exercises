3be0187c63a3e8ff50b164841edfc476a051b412
jdridges <jridges@appdev.yearup.org>
2024-11-01 11:40:12 -0500

    completed math and stats exercise. Compare with classmates to see different ways of completing.

--- Functions/math_and_stats.py
+++ Functions/math_and_stats.py
@@ -9,13 +9,46 @@ radius = random.randint(3,10)
 pi = math.pi
 
 #sum of sample 75 samples from 1 to 100:
-def sum_sample()
+print(sum(vals_sample))
+print('\n')
 
 #avg of 75 sample values
-print(statistics.mean(vals_sample))
+mean_of_small_set = statistics.mean(vals_sample)
+print((round(mean_of_small_set, 2)))
+print('\n')
 
 
 #median of 75 sample values
 print(statistics.median(vals_sample))
+print('\n')
 
 #avg of 200 values
+mean_of_large_set = statistics.mean(vals_choices)
+print(round(mean_of_large_set,2))
+print('\n')
+
+#median of 200 values
+print(statistics.median(vals_choices))
+print('\n')
+
+#mode of 200 values
+print(statistics.mode(vals_choices))
+print('\n')
+
+#std dev of 200 values
+std_dev =statistics.stdev(vals_choices)
+print(round(std_dev, 2))
+print('\n')
+
+#variance of 200 values
+variance_of_large_set= statistics.variance(vals_choices)
+print(round(variance_of_large_set, 2))
+print('\n')
+
+#radius (rounded up)
+radius_of_a_circle_up = math.ceil(radius * 2 * pi)
+print(radius_of_a_circle_up)
+
+#radius(rounded down)
+radius_of_a_circle_down = math.floor(radius*2*pi)
+print(radius_of_a_circle_down)
