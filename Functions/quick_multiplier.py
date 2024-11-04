ca448893ca1a5ab10946d9a216dce7140cb3872e
jdridges <jridges@appdev.yearup.org>
2024-11-03 20:47:17 -0600

    lambda exercise started and added

---
+++ Functions/quick_multiplier.py
@@ -0,0 +1,15 @@
+#doubler variable
+doubler = lambda n: n * 2
+
+
+#printing doubler using 8, -4, 'banana' 
+
+print(doubler(8))
+#returns 16 (as expected)
+
+print(doubler(-4))
+#returns -8(as expected)
+
+print(doubler('banana'))
+#returns bananabanana - prints the string twice (*2)
+
