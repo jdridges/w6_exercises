d27bc294fde3ce8bd282c6c08945e0db43dd4e64
jdridges <jridges@appdev.yearup.org>
2024-11-04 12:05:38 -0600

    completed restaurants exercise working with classes

---
+++ Classes/restaurants.py
@@ -0,0 +1,25 @@
+import datetime
+
+class Restaurant:
+    '''Stores name of Restaurant and types of food Restaurant supplies'''
+
+    def __init__(self, rest_name, food_type):
+        self.rest_name = rest_name
+        self.food_type = food_type
+    
+    def describe_rest(self):
+        print(f'{self.rest_name} serves {self.food_type}.')
+
+    def rest_open(self):
+        print(f'{self.rest_name} is open')
+        
+
+    
+restaurant1 = Restaurant('Weedy\'s', 'American fast food')
+restaurant2 = Restaurant('McNaldos', 'American fast food')
+restaurant3 = Restaurant('Applebapple\'s', 'American fine dining')
+
+for x in [restaurant1, restaurant2, restaurant3]:
+    x.describe_rest()
+    x.rest_open()
+    print()
