597be01f3578819642f578d73a454dd681fc4e9e
jdridges <jridges@appdev.yearup.org>
2024-11-03 22:30:29 -0600

    finished the quick multiplier exercise

--- Functions/quick_multiplier.py
+++ Functions/quick_multiplier.py
@@ -13,3 +13,28 @@ print(doubler(-4))
 print(doubler('banana'))
 #returns bananabanana - prints the string twice (*2)
 
+#tripler variable
+tripler = lambda n: n *3
+
+print(tripler(8))
+print(tripler(-4))
+print(tripler('banana'))
+
+
+def multiplier(multiplier_value):
+    return lambda n: n * multiplier_value
+quadrupler = multiplier(4)
+quintupler = multiplier(5)
+sextupler = multiplier(6)
+septupler = multiplier(7)
+octupler = multiplier(8)
+nonupler = multiplier(9)
+decupler = multiplier(10)
+
+print(f'The number 8 multiplied by 4 is {quadrupler(8)}')
+print(f'-4 multiplied by 5 is {quintupler(-4)}')
+print(f'The word banana sextupled is {sextupler('banana')}')
+print(f'8 septupled is {septupler(8)}')
+print(f'-4 octupled is {octupler(-4)}')
+print(f'The word banana nonupled is {nonupler('banana')}')
+print(f'8 decupled is {decupler(8)}')
